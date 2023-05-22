e0e1593bd7529260a8773b957eb2a880f890b90a
Sergei Zharinov <zharinov@users.noreply.github.com>
2023-05-22 21:04:26 +0300

    Fix tests

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,13 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import * as glob from '..'
 
 test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+  t.is(glob.globToRegex('**/*.ts'), '(?-u)^(?:/?|.*/).*\\.ts$')
+})
+
+test('matching glob pattern', (t) => {
+  t.deepEqual(glob.walkRepoGlob('.', 'package.json'), ['package.json'])
+  t.deepEqual(glob.walkRepoGlobs('.', ['package.json']), ['package.json'])
+  t.deepEqual(glob.walkRepoGlobsMap('.', { package: ['package.json'] }), { package: ['package.json'] })
 })
